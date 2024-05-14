0747fa0aa6d5e5639b844a33eda6031214208b48
Natalialopezutec <natalia.lopez@estudiantes.utec.edu.uy>
2024-05-14 20:10:49 -0300

    me falto

--- 06-JakartaProyectoReferencia/src/main/java/org/tallerjava/moduloGestion/aplicacion/ServicioPago.java
+++ 06-JakartaProyectoReferencia/src/main/java/org/tallerjava/moduloGestion/aplicacion/ServicioPago.java
@@ -1,7 +1,9 @@
 package org.tallerjava.moduloGestion.aplicacion;
 
+import org.tallerjava.moduloGestion.dominio.ClienteTelepeaje;
 import org.tallerjava.moduloGestion.dominio.Cuenta;
 import org.tallerjava.moduloGestion.dominio.Usuario;
+import org.tallerjava.moduloGestion.dominio.Vehiculo;
 
 import java.util.List;
 
@@ -12,4 +14,6 @@ public interface ServicioPago {
     public boolean esClienteTelepeaje(int tag);
     
     public void altaClienteTelepeaje(Usuario usr);
+    
+    public boolean vincularVehiculo(ClienteTelepeaje cliTelepeaje, Vehiculo vehiculo);
 }
